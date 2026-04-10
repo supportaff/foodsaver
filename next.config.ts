d01065558e4b2cdf8adc80b03191e9ed1041c906
Team Afforal <96732336+supportaff@users.noreply.github.com>
2026-04-10 11:00:34 +0530

    Initial commit: FoodSaver MVP - Project foundation, config files

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+};
+
+export default nextConfig;
